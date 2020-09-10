a4d7c183f2bf010a5539efe0a5f51c88667e7bbf
Pat  Pignataro <pat6284@msn.com>
2020-09-10 12:47:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ const [moo, neigh, baa, oink, cluck] = farmAnimals.split(' ')
 
 const [Bessie, ,Dolly, Babe, Little] = farmAnimals.split(' ')
 
-// const [black_and_white, ,black, pink, ,] = farmAnimals.split(' ')
+const [black_and_white, ,black, pink, ,] = farmAnimals.split(' ')
 
 // const [red, orange, yellow, green, blue, indigo, violet] = colors
 
