43f26de93840104b129136faf959ca13e5b1b797
Pat  Pignataro <pat6284@msn.com>
2020-09-10 12:48:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -38,5 +38,5 @@ const [, , , , , indg, ] = colors
 
 const { name, color, song, job, partner } = muppet
 
-const { song_2, song_4 } = k_muppet.k_album.the_muppet_movie
-// const { k_color, k_job, k_partner } = k_muppet
+// const { song_2, song_4 } = k_muppet.k_album.the_muppet_movie
+const { k_color, k_job, k_partner } = k_muppet
